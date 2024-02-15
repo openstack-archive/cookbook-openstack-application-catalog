8c06fcc597aa9d90d21bf9e596c3b1d78471aa85
Ghanshyam Mann <gmann@ghanshyammann.com>
2024-02-15 14:15:39 -0800

    Retire openstack-chef: remove repo content
    OpenStack-chef project is retiring
    - https://review.opendev.org/c/openstack/governance/+/905279
    
    this commit remove the content of this project repo
    
    Depends-On: https://review.opendev.org/c/openstack/project-config/+/909134
    Change-Id: Ia65538ed62d514db9d182c6ca62249a0513aa377

--- libraries/matchers.rb
+++
@@ -1,9 +0,0 @@
-# encoding: UTF-8
-if defined?(ChefSpec)
-  def create_openstack_application_catalog_application(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(
-      :openstack_application_catalog_application,
-      :create,
-      resource_name)
-  end
-end
